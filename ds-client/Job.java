f3945fbfaf0ad09cd47c77740c7013e1fc470379
Eli Mana <elimana97@gmail.com>
2021-03-28 18:36:57 +1100

    add Job.java class

---
+++ ds-client/Job.java
@@ -0,0 +1,18 @@
+public class Job {
+  String submitTime, ID, estRuntime, core, memory, disk;
+  
+  public Job(String s) {
+    String[] parsed = s.split(" ");
+
+    submitTime = parsed[1];
+    ID = parsed[2];
+    estRuntime = parsed[3];
+    core = parsed[4];
+    memory = parsed[5];
+    disk = parsed[6];
+  }
+
+  public Job() {
+    
+  }
+}
