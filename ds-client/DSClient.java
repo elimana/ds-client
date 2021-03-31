c93fffcbd5c78e7660db16d54e7fd6ccd37e5cee
Eli Mana <elimana97@gmail.com>
2021-03-31 12:03:12 +1100

    implement job scheduling functionality

--- ds-client/DSClient.java
+++ ds-client/DSClient.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class DSClient {
 
@@ -15,9 +16,16 @@ public class DSClient {
     try {
       dsclient.connect(50000);
 
-      dsclient.printJobs();
+      List<Server> s = dsclient.getServers();
 
-      dsclient.disconnect();
+
+      Job j = dsclient.getNextJob();
+      while (j != null) {
+        dsclient.dispatch(j, s.get(4));
+        j = dsclient.getNextJob();
+      }
+      
+
     } catch (UnknownHostException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -35,22 +43,87 @@ public class DSClient {
     // Handshake
     this.write("HELO");
       
-    System.out.println(this.read());
+    System.out.println(this.readLine());
 
     this.write("AUTH eli");
     
-    System.out.println(this.read());
+    System.out.println(this.readLine());
 
     this.write("REDY");
 
+    this.readLine();
+
     return client;
+  }
+
+  public List<Server> getServers() {
+    List<Server> servers = new ArrayList<Server>();
+
+    try {
+      this.write("GETS All");
+
+      String data[] = this.readLine().split(" ");
+
+      int lines = Integer.parseInt(data[1]);
+      this.write("OK");
+
+      for (int i = 0; i < lines; i++) {
+        // System.out.println(this.read(32));
+        Server s = new Server(this.readLine());
+        System.out.println(s);
+        servers.add(s);
+      }
+
+      this.write("OK");
+
+      this.readLine();
+
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return servers;
+  }
+
+  public Job getNextJob() {
+    Job j = null;
+
+    try {
+      this.write("REDY");
+
+      String reply = this.readLine();
+      if (reply.equals("NONE")) {
+        this.disconnect();
+        return j;
+      } else if (!reply.split(" ")[0].equals("JOBN")) {
+        return this.getNextJob();
+      } else {
+        j = new Job(reply);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return j;
+  }
+
+  public void dispatch(Job j, Server s) {
+    try {
+      System.out.println("SCHD " + j.ID + " " + s.type + " " + s.ID);
+      this.write("SCHD " + j.ID + " " + s.type + " " + s.ID);
+      System.out.println(this.readLine());
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   public void disconnect() {
     if (client != null) {
       try {
         this.write("QUIT");
-        System.out.println(read());
+        System.out.println(this.readLine());
         client.close();
       } catch (IOException e) {
         // TODO Auto-generated catch block
@@ -60,23 +133,25 @@ public class DSClient {
   }
   
   public String read() throws IOException {
+    return read(32);
+  }
 
-    byte[] b = new byte[32];
+  public String read(int len) throws IOException {
+    byte[] b = new byte[len];
     InputStream in = client.getInputStream();
     in.read(b);
     return new String(b);
   }
 
-  public void write(String message) throws IOException {
-    client.getOutputStream().write(message.getBytes());
-    client.getOutputStream().flush();
+  public String readLine() throws IOException {
+    DataInputStream in = new DataInputStream(client.getInputStream());
+    return in.readLine();
   }
 
-  public void printJobs() throws IOException {
-    String input = this.read();
-    while (!input.equals("NONE")) {
-      System.out.println(input);
-    }
+  public void write(String message) throws IOException {
+    message = message + "\n";
+    client.getOutputStream().write(message.getBytes());
+    client.getOutputStream().flush();
   }
 
 }
