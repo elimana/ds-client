832f01a0a4b6182b657769d1b0ffcdaeacd634e4
mucy677 <danbshek@gmail.com>
2021-04-18 20:13:47 +1000

    Added javadoc comments for server class

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,17 @@
+/**
+ * Constructs and provides interfaces for the server object
+ */
 public class Server implements Comparable<Server>{
   private String type, state;
   private int ID, curStartTime, core, mem, disk, bootTime;
   private float hourlyRate;
 
+  /**
+   * Constructs a server object from a string
+   * 
+   * @param s Constructor input string with server fields delimited by the space
+   *          character
+   */
   public Server (String s) {
     String[] parsed = s.split(" ");
     type = parsed[0];
@@ -16,47 +25,104 @@ public class Server implements Comparable<Server>{
     hourlyRate = Float.parseFloat(parsed[8]);
   }
 
+  /**
+   * Getter function for server type
+   * 
+   * @return server type in string format
+   */
   public String getType() {
     return this.type;
   }
 
+  /**
+   * Getter function for server ID
+   * 
+   * @return server ID in int format
+   */
   public int getID() {
     return this.ID;
   }
 
+  /**
+   * Getter function for server state
+   * 
+   * @return server state in string format
+   */
   public String getState() {
     return this.state;
   }
 
+  /**
+   * Getter function for current start time
+   * 
+   * @return current start time in int format
+   */
   public int getCurStartTime() {
     return this.curStartTime;
   }
 
+  /**
+   * Getter function for server CPU core count
+   * 
+   * @return Number of server CPU cores in int format
+   */
   public int getCore() {
     return this.core;
   }
 
+  /**
+   * Getter function for server memory
+   * 
+   * @return server memory (MB) in int format
+   */
   public int getMem() {
     return this.mem;
   }
 
+  /**
+   * Getter function for server disk space
+   * 
+   * @return server disk space (MB) in int format
+   */
   public int getDisk() {
     return this.disk;
   }
 
+  /**
+   * Getter function for server boot time
+   * 
+   * @return server boot time in int format
+   */
   public int getBootTime() {
     return this.bootTime;
   }
 
+  /**
+   * Getter function for server hourly rate
+   * 
+   * @return server hourly rate in floating point format
+   */
   public float getHourlyRate() {
     return this.hourlyRate;
   }
 
+  /**
+   * Function for displaying server ID and type together, in a friendly format.
+   * Useful for testing.
+   * 
+   * @return Server type followed by server ID, separated by a space character
+   */
   @Override
   public String toString () {
     return type + " " + ID;
   }
 
+  /**
+   * Function for displaying all server attributes together, in a detailed form.
+   * Attributes are separated by a space character. Useful for testing.
+   * 
+   * @return String containing all server details, separated by space characters
+   */
   public String getAllServerDetails() {
 
     String serverDetails = "";
@@ -76,11 +142,11 @@ public class Server implements Comparable<Server>{
   }
 
   /**
-   * First priority to sort cores descending.
-   * Second priority to sort ID ascending.
+   * First priority to sort cores descending. Second priority to sort ID
+   * ascending.
    * 
-   * @param other server to compare to
-   * @return int
+   * @param other server object to compare against
+   * @return int varying according to results of comparison (1 or 0 or -1)
    */
   @Override
   public int compareTo(Server other) {
@@ -100,6 +166,5 @@ public class Server implements Comparable<Server>{
       return 0;
     }
   }
-
 
 }
