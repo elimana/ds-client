bcd9c7ac1c980afd56d0819e57c8dc301d65432f
Eli Mana <elimana97@gmail.com>
2021-05-24 19:52:36 +1000

    add Resource class

---
+++ src/Resource.java
@@ -0,0 +1,35 @@
+public class Resource {
+  private int availableCores, availableMem, availableDisk, pendingJobs;
+
+  public Resource(int availableCores, int availableMem, int availableDisk, int pendingJobs) {
+    this.availableCores = availableCores;
+    this.availableMem = availableMem;
+    this.availableDisk = availableDisk;
+    this.pendingJobs = pendingJobs;
+  }
+
+  public int getAvailableCores() {
+    return this.availableCores;
+  }
+
+  public int getAvailableMem() {
+    return this.availableMem;
+  }
+
+  public int getAvailableDisk() {
+    return this.availableDisk;
+  }
+
+  public int getPendingJobs() {
+    return this.pendingJobs;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+      " availableCores='" + getAvailableCores() + "'" +
+      ", availableMem='" + getAvailableMem() + "'" +
+      ", availableDisk='" + getAvailableDisk() + "'" +
+      "}";
+  }
+}
